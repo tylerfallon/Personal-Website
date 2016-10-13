20be2231db39d6d501736ecfda41c76d5fda882c
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro-2.local>
2016-10-13 15:04:27 -0400

    initial commit

---
+++ app/components/Saved.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var Router = require('react-router')
+
+var Main = React.createClass({
+render: function(){
+
+		return(
+
+			<div className="main-container"> 
+			Hello world!
+			</div>
+
+		)
+	}
+});
+module.exports = Main;
---
+++ app/components/Search.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var Router = require('react-router');
+
+var Search = React.createClass({
+	render: function(){
+
+		return(
+
+			<div className="main-container"> 
+
+				Hello world
+
+			</div>
+
+		)
+	}
+});
+
+module.exports = Search;
